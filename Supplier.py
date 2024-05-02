58ff944b47e6e7ab9e06722397c4fa40251dc66a
NadaAlz <165168524+NadaAlz@users.noreply.github.com>
2024-05-02 19:31:43 +0400

    Update Supplier.py

--- Supplier.py
+++ Supplier.py
@@ -2,6 +2,7 @@ from enum import Enum
 import re
 
 class S_Type(Enum):
+    """Class to represent supplier type as Enum values"""
     C = "Catering"
     CL = "Cleaning"
     F = "Furniture"
@@ -10,6 +11,7 @@ class S_Type(Enum):
 
 class Supplier:
     """Class to represent a supplier"""
+    #Constructor to initialize attributes:
     def __init__(self, s_name='', s_ID=0, s_type=S_Type.C, s_address='', s_contact_details=''):
         self.s_name = s_name
         self.s_ID = s_ID
@@ -17,21 +19,19 @@ class Supplier:
         self.s_address = s_address
         self.s_contact_details = s_contact_details
 
+    #Setter Functions
     def set_s_name(self, s_name):
         self.s_name = s_name
-
     def set_s_ID(self, s_ID):
         self.s_ID = s_ID
-
     def set_s_type(self, s_type):
         self.s_type = s_type
-
     def set_s_address(self, s_address):
         self.s_address = s_address
-
     def set_s_contact_details(self, s_contact_details):
         self.s_contact_details = s_contact_details
 
+    #Getter Functions
     def get_s_name(self):
         return self.s_name
 
@@ -47,6 +47,7 @@ class Supplier:
     def get_s_contact_details(self):
         return self.s_contact_details
 
+    #Function to create a new supplier
     def add_supplier(self, suppliers, s_name, s_ID, s_type, s_address, s_contact_details, c_min_guests=None,
                      c_max_guests=None):
         try:
@@ -100,8 +101,10 @@ class Supplier:
         except ValueError as e:
             return False, str(e)
 
+   #Function to delete a supplier given an ID
     def delete_supplier(self, suppliers, s_ID):
         try:
+            #This checks each supplier in the suppliers' list and deletes the desired supplier
             for supplier in suppliers:
                 if supplier.s_ID == s_ID:
                     suppliers.remove(supplier)
@@ -110,10 +113,12 @@ class Supplier:
         except ValueError:
             return False, "Please enter a valid supplier ID."
 
+    #Function to modify a supplier details given the ID and the chosen detail and the new detail
     def modify_supplier(self, suppliers, s_ID, detail, new_detail):
         try:
             for supplier in suppliers:
                 if supplier.s_ID == s_ID:
+                    #These lines will check what the user wants to modify and then change the detail to the given one
                     if detail == "Name":
                         if not new_detail.replace(" ", "").isalpha():
                             raise ValueError("Name must contain only letters and spaces.")
@@ -127,15 +132,19 @@ class Supplier:
                             raise ValueError("Contact details must be a phone number with a length of 9 digits.")
                         supplier.s_contact_details = new_detail
 
+                    #The details are changed
                     return True, f"Supplier {detail} modified successfully."
+            #The supplier with this ID does not exist
             return False, "Supplier not found."
         except ValueError as e:
             return False, str(e)
 
+    #Function to display a supplier details given the ID
     def display_supplier(self, suppliers, s_ID):
         try:
             for supplier in suppliers:
                 if supplier.s_ID == s_ID:
+                    #This will go through each detail and print it
                     details = f"Name: {supplier.s_name}\nID: {supplier.s_ID}\nType: {supplier.s_type.value}\n" \
                               f"Address: {supplier.s_address}\nContact Details: {supplier.s_contact_details}"
                     return True, details
@@ -144,12 +153,16 @@ class Supplier:
             return False, "Please enter a valid supplier ID."
 
 class Catering_Company(Supplier):
-    """Class to represent a catering company"""
+    """Class to represent a catering company as a child of Supplier Class """
+
+    #Constructor
     def __init__(self, s_name='', s_ID='', s_type=S_Type.C, s_address='', s_contact_details='', menu=[], c_min_guests=0, c_max_guests=0):
-        super().__init__(s_name, s_ID, s_type, s_address, s_contact_details)
+        super().__init__(s_name, s_ID, s_type, s_address, s_contact_details) #Inheritance
         self.menu = menu
         self.c_min_guests = c_min_guests
         self.c_max_guests = c_max_guests
+
+   #Setter Functions
 
     def set_menu(self, menu):
         self.menu = menu
@@ -160,6 +173,7 @@ class Catering_Company(Supplier):
     def set_c_max_guests(self, c_max_guests):
         self.c_max_guests = c_max_guests
 
+    #Getter Functions
     def get_menu(self):
         return self.menu
 
@@ -169,10 +183,3 @@ class Catering_Company(Supplier):
     def get_c_max_guests(self):
         return self.c_max_guests
 
-class Cleaning_Company(Supplier):
-    def __init__(self, s_name='', s_ID='', s_type=S_Type.C, s_address='', s_contact_details=''):
-        super().__init__(s_name, s_ID, s_type, s_address, s_contact_details)
-
-class Decorations_Company(Supplier):
-    def __init__(self, s_name='', s_ID='', s_type=S_Type.C, s_address='', s_contact_details=''):
-        super().__init__(s_name, s_ID, s_type, s_address, s_contact_details)
